a66e506774e9c9c538185493f0e702fb6851fdcb
GMELUM <gmelum@vk.com>
2026-07-21 08:11:15 +0300

    revrite && optimize

--- adapters/fiber/fiber_bench_test.go
+++ adapters/fiber/fiber_bench_test.go
@@ -2,13 +2,37 @@ package fiber
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 
 	etp "github.com/elum-utils/go-etp"
 )
 
-func BenchmarkReadETPFrameLease1KiB(b *testing.B) {
-	frame, err := etp.EncodeFrame(etp.NewFrame(etp.FrameRequest, etp.SchemaEvent, make([]byte, 1024)))
+func BenchmarkReadETPFrameLease(b *testing.B) {
+	for _, size := range []int{1 << 10, 8 << 10, 16 << 10, 32 << 10, 64 << 10} {
+		b.Run(fmt.Sprintf("%dKiB", size>>10), func(b *testing.B) {
+			frame, err := etp.EncodeFrame(etp.NewFrame(etp.FrameRequest, etp.SchemaEvent, make([]byte, size)))
+			if err != nil {
+				b.Fatal(err)
+			}
+			reader := bytes.NewReader(frame)
+
+			b.ReportAllocs()
+			b.SetBytes(int64(len(frame)))
+			for b.Loop() {
+				reader.Reset(frame)
+				lease, err := readETPFrameLease(reader, false)
+				if err != nil {
+					b.Fatal(err)
+				}
+				lease.Release()
+			}
+		})
+	}
+}
+
+func BenchmarkReadETPFrameLeaseStrict(b *testing.B) {
+	frame, err := etp.EncodeFrame(etp.NewFrame(etp.FrameRequest, etp.SchemaEvent, make([]byte, 8<<10)))
 	if err != nil {
 		b.Fatal(err)
 	}
@@ -18,7 +42,7 @@ func BenchmarkReadETPFrameLease1KiB(b *testing.B) {
 	b.SetBytes(int64(len(frame)))
 	for b.Loop() {
 		reader.Reset(frame)
-		lease, err := readETPFrameLease(reader, false)
+		lease, err := readETPFrameLease(reader, true)
 		if err != nil {
 			b.Fatal(err)
 		}
